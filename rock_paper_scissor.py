1349c014f5fa9a1371536f29b95c220432b2c6dd
ngocpham1994x <ngoc.pham.nm@gmail.com>
2022-09-05 13:43:26 -0500

    completed

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,6 +1,24 @@
+import random as rd
+
+
+def rps():
+    user = input("What you choice? 'rock', 'paper', 'scissor'?  ")
+    computer = rd.choice(['rock', 'paper', 'scissor'])
+
+    print (f"Computer choice is:  {computer}")
+
+    if user == computer:
+        print ("It's a tie.")
+    elif (user == 'rock' and computer == 'scisssor') or \
+        (user == 'paper' and computer == 'rock') or \
+        (user == 'scissor' and computer == 'paper'):
+        print ("Yayy, User wins !")
+    else:
+        print ("Computer wins")
+
 def main():
 
-    print('====================================')
+    rps()
 
 if __name__ == '__main__':
     main()
