b83b5f315c1ed6a4ca1a6e04ae2aafba02bad4da
NgocPham <ngoc.pham.nm@gmail.com>
2022-09-29 23:46:08 -0500

    added get_move() for human player

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -13,6 +13,7 @@ class ComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
+        # get a random valid spot for our next move
         square = rd.choice(game.available_moves())  # square = []
         return square
 
@@ -21,5 +22,13 @@ class HumanPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        return super().get_move(game)
+        valid_square = false
+        val = None
+        while not valid_square:
+            square = input (self. letter + '\'s turn. Input move (0-9): ')
+            # we're going to check that this is a correct value by trying to cast it to an integer,
+            # and if it's not, then we say it's invalid.
+            # if that spot is not available on the board, we also say it's invalid
+            
+        # return super().get_move(game)
 
